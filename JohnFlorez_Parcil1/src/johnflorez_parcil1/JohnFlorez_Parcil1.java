3121232b24d2284dae1e1f268785526efc1c619f
johnandreiflorez <johnandreiflorez@gmail.com>
2020-01-24 10:20:09 -0500

    terminado

--- JohnFlorez_Parcil1/src/johnflorez_parcil1/JohnFlorez_Parcil1.java
+++ JohnFlorez_Parcil1/src/johnflorez_parcil1/JohnFlorez_Parcil1.java
@@ -1,12 +1,15 @@
 
 package johnflorez_parcil1;
 
+import vista.Colilla_Pago;
+
 
 public class JohnFlorez_Parcil1 {
 
 
     public static void main(String[] args) {
-        
+        Colilla_Pago objColilla_Pago = new Colilla_Pago();
+        objColilla_Pago.setVisible(true);
     }
     
 }
